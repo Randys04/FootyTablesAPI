d4798c72ed588e1bfdb9d95e0b6eff3f9e1a2f84
Randy04 <stevenmoya02@gmail.com>
2024-07-15 19:37:41 -0600

    FootyTablesContext with dependency injection was registered
    Microsoft.AspNetCore.Identity.EntityFrameworkCore package was added

--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
+using FootyTablesAPI;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("ConnectionStr");
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<FootyTablesContext>(
+        options => options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
 
 var app = builder.Build();
 
